3bbb5402eab30e19ad328654b81367f61e559e6d
selam ghiwot <ghiwotselam@gmail.com>
2021-01-26 11:27:33 +0300

    index page template done

---
+++ src/components/avatarComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "avatarComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/carouselComponent.vue
@@ -0,0 +1,66 @@
+<template>
+<q-card>
+   <q-carousel
+      swipeable
+      animated
+      v-model="slide"
+      thumbnails
+      infinite
+      arrows
+      style="max-height:250px"
+      :hidden="hidden"
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </q-carousel>
+    <q-card-section v-if="hidden===true">
+    <q-card-title  >
+       Lorem ipsum
+    </q-card-title>
+    </q-card-section>
+ <q-card-section v-if="hidden===true"  >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+         Itaque voluptatem totam, architecto cupiditate officia rerum,
+         error dignissimos praesentium libero ab nemo.<br/>
+         <q-btn class="bg-primary text-white q-mr-lg">read more</q-btn>
+      </q-card-section>
+      <q-card-section :hidden="hidden" >
+        <div class=" text-secondary "> <div>Addis ababa, Ethiopia</div> <q-space/>
+         <q-btn class="bg-primary text-white q-mr-lg">rent</q-btn></div>
+      </q-card-section>
+    </q-card>
+</template>
+
+<script>
+export default {
+  name: 'EssentialLink',
+  props: {
+    hidden: {
+      type: Boolean,
+      required: false,
+      default:false
+    },
+
+    caption: {
+      type: String,
+      default: ''
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+    data () {
+    return {
+      slide:1
+    }
+    }
+}
+</script>
